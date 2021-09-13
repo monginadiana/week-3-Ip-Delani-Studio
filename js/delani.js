19238d913536d48bd116c035cd69441aaffa83f2
monginadiana <diana.ondieki@student.moringa.com>
2021-09-13 07:59:35 +0300

    changes in the pic1 function in js

--- js/delani.js
+++ js/delani.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(".do").addClass('text-center');
     $(".bold").addClass('text-center');
-    $(".img1").click(function(){
+    $(".pic1").click(function(){
         $(".img1").hide(1000);
         $(".par1").toggle(1000);
     });
